7acbada5c4688453ba6dd696db909af4f208fe9a
WafiWadud <wafinashwan@gmail.com>
2023-11-23 20:47:11 +0600

    Why are there so many bugs!

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 """Url Shortner Frontend And Backend"""
-from os import listdir
+from os.path import exists
 from nicegui import ui
 from random import randbytes
 from base64 import b64encode
+
+
 @ui.page("/app")
 def app() -> None:
     input_field = ui.input(label="Enter A Url")
     link_label = ui.label(text="")
+
     def on_enter():
         shortened_link = makelink(input_field.value)
         link_label.set_text(shortened_link)
     input_field.on("keydown.enter", on_enter)
     link_label = ui.label(text="")
 
+
 def makelink(link) -> str:
     name = b64encode(randbytes(6)).decode()
-    if name and name + ".py" in listdir():
-        return
-    with open(file=name + ".py", mode="w") as file:
-        file.writelines(f"from nicegui import ui\n@ui.page(\"{name}\")\ndef {name}: ui.open(\"{link}\")")
+    content = ""
+    if exists("urls.txt"):
+        with open("urls.txt", "r") as file:
+            content = file.read()
+        if name in content:
+            makelink(link)
+        else:
+            with open(__file__, "a") as file:
+                file.writelines(["from nicegui import ui\n", f"@ui.page(\"/link/{name}\")", f"def {name}: ui.open({link})"])
+
+    with open("urls.txt", "a") as file:
+        file.write(name)
     return name
+
 
 ui.run(title="Url Shortener")
